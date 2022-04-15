767ac853836838474a96fe33a99afc9b2f78a446
Chaitra Dangat <chadangat@gmail.com>
2022-04-15 15:50:45 +0530

    database service updates

--- SinglePageApp.Services/Database/DbService.cs
+++ SinglePageApp.Services/Database/DbService.cs
@@ -1,17 +1,56 @@
-﻿using System;
+﻿using SinglePageApp.Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SinglePageApp.Services.Database
 {
-    public class DbService
+    public class DbService : IDbService
     {
-        public DbService()
+        private readonly UserContext _context;
+
+        public DbService(UserContext context)
         {
+            _context = context;
+        }
+
+        public List<UserInfo> GetAllUsers()
+        {
+            return _context.UserInfos.ToList();
+        }
+
+        public UserInfo GetUserById(int id)
+        {
+            return _context.UserInfos.Find(id);
+        }
+
+        public void GetUserByLogin(string username)
+        {
+            _context.UserInfos.Where(x => x.UserName == username).FirstOrDefault();
+        }
+
+        public void UpdateUsers(List<UserInfo> users)
+        {
+            foreach (var user in users)
+            {
+                UserInfo entity = _context.UserInfos.Find(user.ID);
+
+                if ( entity != null)
+                {
+                    _context.UserInfos.Update(user);       
+                }
+            }
+
+            _context.SaveChanges();
 
         }
 
+        public void UpdateUser(UserInfo user)
+        {
+            _context.Update(user);
 
-
+            _context.SaveChanges();
+        }
     }
 }
---
+++ SinglePageApp.Services/Database/IDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinglePageApp.Services.Database
+{
+    public interface IDbService
+    {
+        public void GetUserByLogin(string username);
+    }
+}
